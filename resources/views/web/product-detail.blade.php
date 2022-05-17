3f02ac7fe342a4784cfed6d42e9ab0867ca2c7b4
dũng hứa <60870815+dunghuava@users.noreply.github.com>
2022-05-17 17:38:57 +0700

    m

--- resources/views/web/product-detail.blade.php
+++ resources/views/web/product-detail.blade.php
@@ -6,7 +6,7 @@
             <div class="col-lg-12">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Trang chủ</a></li>
+                        <li class="breadcrumb-item"><a href="/">Trang chủ</a></li>
                         <li class="breadcrumb-item active" aria-current="page">Sản phẩm</li>
                     </ol>
                 </nav>
