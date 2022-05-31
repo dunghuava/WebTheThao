5c1f76e04339ccbd91195c8e7fe5387764d9f335
dũng hứa <60870815+dunghuava@users.noreply.github.com>
2022-05-31 21:08:44 +0700

    a

---
+++ resources/views/admin/post/create.blade.php
@@ -0,0 +1,12 @@
+@extends('admin.layout')
+@section('title','page')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/admin/sidebar.blade.php
@@ -0,0 +1,81 @@
+<nav class="top-nav pt-4 pb-2">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <a href="/">
+                    <img height="80px" src="/images/logo.png"/>
+                </a>
+            </div>
+            <div class="col-lg-9">
+                <div class="row top-search">
+                    <div class="col-lg-8">
+                        <form class="form-group form-inline">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Tìm kiếm....">
+                                <div class="input-group-append">
+                                    <button class="btn btn-success" type="submit">Tìm</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="col-lg-4 text-right">
+                        <a href="/gio-hang" class="btn btn-danger">Giỏ hàng (0)</a>
+                    </div>
+                </div>
+                <ul class="menu-top mb-0">
+                    <li>
+                        <a href="/">Trang chủ</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/trang-phuc-the-thao">Trang phục thể thao</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/thiet-bi-tap">Thiết bị tập</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/tnis">Tenis</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/gold">Gold</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/bong-da">Bóng đá</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/tin-tuc">Tin tức</a>
+                    </li>
+                    <li>
+                        <a href="/danh-muc/lien-he">Liên hệ</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
+<section id="carousel" class="carousel slide mb-2" data-ride="carousel">
+    <!-- Indicators -->
+    <ul class="carousel-indicators">
+        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
+    </ul>
+    <!-- The slideshow -->
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="/images/banner.png" width="100%" height="500">
+        </div>
+        <div class="carousel-item">
+            <img src="/images/banner.png" width="100%" height="500">
+        </div>
+        <div class="carousel-item">
+            <img src="/images/banner.png" width="100%" height="500">
+        </div>
+    </div>
+    <!-- Left and right controls -->
+    <a class="carousel-control-prev" href="#carousel" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+    </a>
+    <a class="carousel-control-next" href="#carousel" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+    </a>
+</section>
